7731e4e761d32c5a66f567bee191ea85d45402e1
unknown <miodrag.maric86@gmail.com>
2018-04-06 22:32:58 +0200

    ready

--- server/routes/router.js
+++ server/routes/router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/*', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../../dist/index.html'));
 });
 
