b03092a2080708016fb074ba861cb9c3a1549cac
unknown <miodrag.maric86@gmail.com>
2018-04-06 22:14:03 +0200

    ready

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'dist/index.html')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/', router);
 
